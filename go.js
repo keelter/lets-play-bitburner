106e8be2042fd8de25d9e7c2a46a4e36a29c0dc0
KEELTER <122953063+keelter@users.noreply.github.com>
2023-11-20 00:16:56 -0500

    Update go.js
    v2

--- go.js
+++ go.js
@@ -1,77 +1,76 @@
-/** @param {NS} ns **/
-
 /**
- * returns an array of servers dynamically
+ * Returns an array of servers dynamically.
  */
-function dpList(ns, current="home", set=new Set()) {
-	let connections = ns.scan(current)
-	let next = connections.filter(c => !set.has(c))
-	next.forEach(n => {
-		set.add(n);
-		return dpList(ns, n, set)
-	})
-	return Array.from(set.keys())
+function dpList(ns, current = "home", set = new Set()) {
+    let connections = ns.scan(current);
+    let next = connections.filter(c => !set.has(c));
+    next.forEach(n => {
+        set.add(n);
+        return dpList(ns, n, set);
+    });
+    return Array.from(set);
 }
 
+/**
+ * Calculates the number of threads based on available RAM.
+ */
 function threadCount(ns, hostname, scriptRam) {
-	let threads = 0;
-	let free_ram = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
+    let threads = 0;
+    let free_ram = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname);
 
-	threads = free_ram / scriptRam
-	return Math.floor(threads)
+    threads = free_ram / scriptRam;
+    return Math.floor(threads);
 }
 
 export async function main(ns) {
-	let servers = dpList(ns)
-	let target = "foodnstuff"
-	ns.disableLog("ALL");
-	for (let server of servers) {
-		await ns.scp(["bin.wk.js", "bin.hk.js", "bin.gr.js"], "home", server)
-	}
+    let servers = dpList(ns);
+    let target = "foodnstuff";
+    ns.disableLog("ALL");
+    for (let server of servers) {
+        await ns.scp(["bin.wk.js", "bin.hk.js", "bin.gr.js"], "home", server);
+    }
 
-	while(true) {
-		for (let server of servers) {
-			if (ns.hasRootAccess(server) && ns.hasRootAccess(target)) {
-				// divert all of this server's available threads to the most valuable command
-				if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
-					let available_threads = threadCount(ns, server, 1.75)
-					// weaken the target while security > minsecurity
-					if (available_threads >= 1) {
-						ns.exec("bin.wk.js", server, available_threads, target)
-					}
-				} else if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target)) {
-					let available_threads = threadCount(ns, server, 1.75)
+    while (true) {
+        for (let server of servers) {
+            if (ns.hasRootAccess(server) && ns.hasRootAccess(target)) {
+                // divert all of this server's available threads to the most valuable command
+                if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+                    let available_threads = threadCount(ns, server, 1.75);
+                    // weaken the target while security > minsecurity
+                    if (available_threads >= 1) {
+                        ns.exec("bin.wk.js", server, available_threads, target);
+                    }
+                } else if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target)) {
+                    let available_threads = threadCount(ns, server, 1.75);
 
-					// grow the target while money < maxmoney
-					if (available_threads >= 1) {
-						ns.exec("bin.gr.js", server, available_threads, target)
-					}
-				} else {
-					let available_threads = threadCount(ns, server, 1.7)
+                    // grow the target while money < maxmoney
+                    if (available_threads >= 1) {
+                        ns.exec("bin.gr.js", server, available_threads, target);
+                    }
+                } else {
+                    let available_threads = threadCount(ns, server, 1.7);
 
-					// hack the target
-					if (available_threads >= 1) {
-						ns.exec("bin.hk.js", server, available_threads, target)
-					}
-				}
+                    // hack the target
+                    if (available_threads >= 1) {
+                        ns.exec("bin.hk.js", server, available_threads, target);
+                    }
+                }
+            } else {
+                // open all possible ports on every server; then attempt to nuke the server
+                try {
+                    ns.brutessh(server);
+                    ns.ftpcrack(server);
+                    ns.relaysmtp(server);
+                    ns.httpworm(server);
+                    ns.sqlinject(server);
+                } catch {}
 
-			} else {
-				// open all possible ports on every server; then attempt to nuke the server
-				try {
-					ns.brutessh(server)
-					ns.ftpcrack(server)
-					ns.relaysmtp(server)
-					ns.httpworm(server)
-					ns.sqlinject(server)
-				} catch {}
-				
-				try {
-					ns.nuke(server)
-				} catch {}
+                try {
+                    ns.nuke(server);
+                } catch {}
+            }
 
-			}
-
-		await ns.sleep(10)
-		}
-	}
+            await ns.sleep(10);
+        }
+    }
 }
